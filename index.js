3e0348cbacc43794dc02b10a9e4e73c93ff5d17c
Lex <alexisjaynedefoe@gmail.com>
2017-11-07 13:21:06 +0000

    first commmit

---
+++ index.js
@@ -0,0 +1,30 @@
+var results = document.getElementById('results');
+var searchResults = document.getElementById('search-results');
+var submit = document.getElementById('button');
+
+var gifObject;
+var gifUrl;
+
+searchGifs();
+function clicked() {
+  submit.addEventListener('click', searchGifs);
+};
+
+function searchGifs() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      gifObject = JSON.parse(xhr.responseText);
+      gifUrl = gifObject.data.image_url;
+      image.src = gifUrl;
+    }
+  };
+
+  xhr.open("GET", "https://api.giphy.com/v1/gifs/random?api_key=OlQPATAEpuqDQXxluhspjJNoqHtQjSFK&tag=rupauls+drag+race?", true);
+  xhr.send();
+
+
+  console.log("click");
+
+}
+clicked();
